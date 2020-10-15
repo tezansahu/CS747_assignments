8fc6c8dd23a11bc634f479117b2f12d6696f295c
Tezan Sahu <tezansahu@gmail.com>
2020-10-15 16:16:41 +0530

    completed LP

--- cs747-pa2/planner.py
+++ cs747-pa2/planner.py
@@ -1,5 +1,11 @@
+"""
+MDP Planning using Value Iteration, Howard's Policy Iteration & Linear Programming
+Author: Tezan Sahu [170100035]
+"""
+
 import argparse
 import numpy as np
+import pulp
 
 parser = argparse.ArgumentParser()
 
@@ -44,22 +50,25 @@ class MDPPlanning:
                 self.T[int(line[1]), int(line[2]), int(line[3])] = float(line[5])
 
 
-    ################################################
-    ## Implement Value Iteration for MDP Planning ##
-    ################################################
     def valueIteration(self, threshold = 10e-12):
+        """
+        Function to implement Value Iteration for MDP Planning
+
+        'threshold' is the small number threshold to signal convergence of the value function
+        """
+
         V_old = np.zeros(self.num_states)
         V = np.zeros(self.num_states)
         pi = np.zeros(self.num_states)
         while True:
-            V_temp = np.zeros((self.num_states, self.num_actions))
+            Q = np.zeros((self.num_states, self.num_actions))
             for s in range(self.num_states):
                 for a in range(self.num_actions):
                     for s_prime in range(self.num_states):
-                        V_temp[s, a] += self.T[s, a, s_prime] * (self.R[s, a, s_prime] + self.discount*V_old[s_prime])
+                        Q[s, a] += self.T[s, a, s_prime] * (self.R[s, a, s_prime] + self.discount*V_old[s_prime])
             
-            V = np.max(V_temp, axis = 1)
-            pi = np.argmax(V_temp, axis = 1)
+            V = np.max(Q, axis = 1)
+            pi = np.argmax(Q, axis = 1)
 
             if np.max(np.abs(V - V_old)) < threshold:
                 break
@@ -69,22 +78,63 @@ class MDPPlanning:
         return V, pi
 
 
-    ###################################################
-    ## Implement Linear Programming for MDP Planning ##
-    ###################################################
     def linearProgramming(self):
-        V = np.zeros(self.num_states)
-        pi = np.zeros(self.num_states)
+        """
+        Function to implement Linear Programming for MDP Planning
 
-        # Logic to be filled
+        """
+        prob = pulp.LpProblem('MdpPlanning', pulp.LpMinimize)
+        
+        # Define the decisio variables & objective function to minimize
+        decision_variables = []
+        objective_func = ""
+        for s in range(self.num_states):
+            variable = str('V_' + str(s))
+            variable = pulp.LpVariable(str(variable))
+            decision_variables.append(variable)
 
-        return V, pi
+            objective_func += variable
+        
+        prob += objective_func
+
+        # Define the constraints for the MDP Planning LP
+        for s in range(self.num_states):
+            for a in range(self.num_actions):
+                constraint = ""
+                for s_prime in range(self.num_states):
+                    constraint += self.T[s, a, s_prime] * (self.R[s, a, s_prime] + self.discount*decision_variables[s_prime])
+                prob += (decision_variables[s] >= constraint)
+
+        # Solve the LP
+        optimization_result = prob.solve(pulp.PULP_CBC_CMD(msg=0))
+        assert optimization_result == pulp.LpStatusOptimal
+
+        # Parse the output of the solver to get V*
+        V_star = np.zeros(self.num_states)
+        for v in prob.variables():
+            s = int(v.name.replace("V_", ""))
+            V_star[s] = v.varValue
+        
+        # Use V* to get Q*
+        Q_star = np.zeros((self.num_states, self.num_actions))
+        for s in range(self.num_states):
+            for a in range(self.num_actions):
+                for s_prime in range(self.num_states):
+                    Q_star[s, a] += self.T[s, a, s_prime] * (self.R[s, a, s_prime] + self.discount*V_star[s_prime])
+        
+        # Use Q* to get pi*
+        pi_star = np.argmax(Q_star, axis = 1)
+
+        return V_star, pi_star
 
 
-    ##########################################################
-    ## Implement Howard's Policy Iteration for MDP Planning ##
-    ##########################################################
     def howardsPolicyIteration(self, threshold=10e-12):
+        """
+        Function to implement Howard's Policy Iteration for MDP Planning
+
+        'threshold' is the small number threshold to signal convergence of the value function
+        """
+
         V = np.zeros(self.num_states)
         pi = np.zeros(self.num_states, dtype=np.int64)
 
@@ -99,10 +149,17 @@ class MDPPlanning:
         return V, pi
     
 
-    ############################
-    ## Policy Evaluation Step ##
-    ############################
     def __policyEvaluation(self, V, pi, threshold=10e-12):
+        """
+        Internal function for the Policy Evaluation Step of Howard's Policy Iteration
+
+        'V' is the current value function
+
+        'pi' is the current policy followed
+
+        'threshold' is the small number threshold to signal convergence of the value function
+        """
+
         while True:
             V_temp = np.zeros(self.num_states)
             for s in range(self.num_states):
@@ -117,10 +174,17 @@ class MDPPlanning:
         return V
     
 
-    #############################
-    ## Policy Improvement Step ##
-    #############################
     def __policyImprovement(self, V, pi, threshold=10e-12):
+        """
+        Internal function for the Policy Improvement Step of Howard's Policy Iteration
+
+        'V' is the current value function
+
+        'pi' is the current policy followed
+        
+        'threshold' is the small number threshold to signal convergence of the value function
+        """
+
         Q = np.zeros((self.num_states, self.num_actions))
         improvable_states = []
         improvable_actions = {}
@@ -143,15 +207,22 @@ class MDPPlanning:
         return pi, True
 
 
-    ##########################################################################
-    ## Print optimal value function & optimal policy in the required format ##
-    ##########################################################################
     def printResults(self, V_star, pi_star):
+        """
+        Function to print optimal value function & optimal policy in the required format
+
+        'V_star' is the optimal value function for the MDP
+
+        'pi_star' is the optimal policy for the MDP
+        """
+
         for (V_s, pi_s) in zip(V_star, pi_star):
             print(V_s, "\t", pi_s, "\n")
 
 
-
+###########################################################################################################################
+####################################################### Main Program ######################################################
+###########################################################################################################################
 def main():
     parser.add_argument("--mdp", type=str)
     parser.add_argument("--algorithm", type=str)
@@ -162,10 +233,10 @@ def main():
 
     if args.algorithm == "vi":
         V_star, pi_star = mdp.valueIteration()
-    elif args.algorithm == "lp":
-        V_star, pi_star = mdp.linearProgramming()
     elif args.algorithm == "hpi":
         V_star, pi_star = mdp.howardsPolicyIteration()
+    elif args.algorithm == "lp":
+        V_star, pi_star = mdp.linearProgramming()
     else:
         raise(Exception("Invalid algorithm input"))
     
